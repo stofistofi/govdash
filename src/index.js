53e17d1d939ac903cb9dfe3aa5132e7d159900cd
stofistofi <kristofer17@ru.is>
2020-05-02 23:22:08 +0000

    serviceWorker hidden

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
 ReactDOM.render(
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="/demo/star-admin-free/react/template/demo_1/preview/">
     <App />
   </BrowserRouter>
 , document.getElementById('root'));
 
-serviceWorker.unregister();
+//serviceWorker.unregister();
