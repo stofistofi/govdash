930e31534d66071def855950642f28e0a2a03f60
stofistofi <kristofer17@ru.is>
2020-05-02 21:38:27 +0000

    more amplify packages added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
 import * as serviceWorker from './serviceWorker';
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 ReactDOM.render(
   <BrowserRouter basename="/demo/star-admin-free/react/template/demo_1/preview">
